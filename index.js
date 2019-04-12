328146bc8c8280da6d5fbb1bb1bead449d05885f
Ben Grabowski <gbowski30@gmail.com>
2019-04-12 16:03:59 -0600

    added generateItemElement function

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ $(function() {
 
         let itemName = $(this).find('input [name="shopping-list-entry"]').val();
 
-        $("#shopping-list").append(
-            <li>
-                <span class="shopping-item">itemName</span>
-                <div class="shopping-item-controls">
-                    <button class="shopping-item-toggle">
-                        <span class="button-label">check</span>
-                    </button>
-                    <button class="shopping-item-delete">
-                        <span class="button-label">delete</span>
-                    </button>
-                </div>
-            </li>);
+       let newItem = function generateItemElement(itemName) {
+            return `<li>
+            <span class="shopping-item">itemName</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+         </li>`
+        };
+        
+        $("#shopping-list").append(newItem);
     });
 
     $(".shopping-item-controls").on('click', '.shopping-item-toggle', function(event) {
